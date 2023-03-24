cdf2a37389d8f12b65e5c040947ff1c8fc0ddf36
Adriano Carvalho Batista <beregueder@gmail.com>
2023-03-23 23:29:18 -0300

    01-22 Categories Pagination

--- backend/src/main/java/com/estudos/spring/backend/resources/CategoryResource.java
+++ backend/src/main/java/com/estudos/spring/backend/resources/CategoryResource.java
@@ -3,12 +3,14 @@ package com.estudos.spring.backend.resources;
 import com.estudos.spring.backend.dto.CategoryDTO;
 import com.estudos.spring.backend.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -16,20 +18,26 @@ public class CategoryResource {
 
     @Autowired
     private CategoryService service;
+
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findlAll(){
-        List<CategoryDTO> list = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findlAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<CategoryDTO> findlById(@PathVariable Long id){
+    public ResponseEntity<CategoryDTO> findlById(@PathVariable Long id) {
         CategoryDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     }
 
     @PostMapping
-    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
 
         dto = service.insert(dto);
 
@@ -40,17 +48,17 @@ public class CategoryResource {
 
     }
 
-   @PutMapping(value = "/{id}")
-    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
 
-        dto = service.update(id,dto);
+        dto = service.update(id, dto);
 
-       return ResponseEntity.ok().body(dto);
+        return ResponseEntity.ok().body(dto);
 
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<CategoryDTO> delete(@PathVariable Long id){
+    public ResponseEntity<CategoryDTO> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
